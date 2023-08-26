2783c483ca6ed9d1c78814a02502740c6810bf81
Phat-Nguyen-2411 <phat24112411@gmail.com>
2023-08-26 22:49:48 +0700

    change date

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.SQLOutput;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int number = 1;
+        byte numberByte = 1;
+        float numberFloat = 1;
+        double numberDouble = 1.2;
+        boolean booleanResult = true;
+        System.out.println(number);
+        System.out.println(numberFloat);
+        System.out.println(numberByte);
+        System.out.println(numberDouble);
+        System.out.println(booleanResult);
+    }
+}
